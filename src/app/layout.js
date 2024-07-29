1ab4eafc6d4dae9a363c07f97a64e938232da233
Hairun Huang <hairunhuang@gmail.com>
2024-07-28 19:29:21 -0700

    add google analytic

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,8 +11,28 @@ const mulish = Mulish({ subsets: ['latin'] });
 export const metadata = constructMetadata();
 
 export default function RootLayout({ children }) {
+  const googleAnalyticsId = process.env.GOOGLE_ANALYTICS;
   return (
     <html lang="en" className='light !scroll-smooth'>
+      <head>
+        {googleAnalyticsId && (
+          <>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}></script>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${googleAnalyticsId}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+              }}
+            />
+          </>
+        )}
+      </head>
       <body className={cn('min-h-screen font-sans antialiased', mulish.className)}>
         <Providers>
           <Toaster />
