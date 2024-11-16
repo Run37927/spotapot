278e9ddbf873f10cc84442fea339df625af7970f
Hairun Huang <hairunhuang@gmail.com>
2024-11-16 12:25:44 +0800

    renew

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,10 @@ import MapList from "@/components/MapList";
 export default function Home() {
   return (
     <>
-      {/* <MapList /> */}
-      <h1 className="font-bold text-center text-5xl">Discontinued due to Spamming</h1>
+      <MapList />
 
       {/* corner popup */}
-      {/* <div className="fixed top-0 left-0 bg-white/75 border border-gray-200 rounded-br-xl h-12 w-fit px-4 py-1 z-40">
+      <div className="fixed top-0 left-0 bg-white/75 border border-gray-200 rounded-br-xl h-12 w-fit px-4 py-1 z-40">
         <a
           href="https://x.com/HairunHuang"
           target="_blank"
@@ -21,7 +20,7 @@ export default function Home() {
           className="text-center text-sm block leading-tight">
           🪲<span className="hover:underline hover:underline-offset-2 ml-1">bug report</span>
         </a>
-      </div> */}
+      </div>
     </>
   );
 }
